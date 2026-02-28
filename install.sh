47e9f26eaf6cd83fd675736e30c418efbf40889c
RyanEx <rian.hendriana13@gmail.com>
2026-03-01 01:09:56 +0700

    Create install.sh
    Script untuk instalasi

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Installing Image Assets Decrypter..."
+
+pkg update -y
+pkg upgrade -y
+pkg install -y python git libjpeg-turbo libpng
+
+pip install pillow
+
+termux-setup-storage
+
+mkdir -p /storage/emulated/0/ryanex
+cd /storage/emulated/0/ryanex
+
+git clone https://github.com/rianhendriana13-collab/image-assets-decrypter.git repo_tmp
+
+cp repo_tmp/image_decrypter_v4.py .
+rm -rf repo_tmp
+
+echo ""
+echo "Install selesai."
+echo "Jalankan dengan:"
+echo "cd /storage/emulated/0/ryanex"
+echo "python image_decrypter_v4.py"
